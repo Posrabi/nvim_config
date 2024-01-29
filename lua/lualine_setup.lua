74ee5d22e0cc7dadf976eff4d2b93c0b2dde7edd
Tri Dao <daominhtri0503@gmail.com>
2024-01-29 11:29:53 -0500

    show relative path in status line

--- lua/lualine_setup.lua
+++ lua/lualine_setup.lua
@@ -1,24 +1,33 @@
-require("lualine").setup{
+require("lualine").setup {
   tabline = {
     lualine_a = {
-     {
-      'tabs',
-      mode = 2,
-      max_length = vim.o.columns,
-      use_mode_colors = true,
-      fmt = function(name, context)
-        -- Show + if buffer is modified in tab
-        local buflist = vim.fn.tabpagebuflist(context.tabnr)
-        local winnr = vim.fn.tabpagewinnr(context.tabnr)
-        local bufnr = buflist[winnr]
-        local mod = vim.fn.getbufvar(bufnr, '&mod')
+      {
+        'tabs',
+        mode = 2,
+        max_length = vim.o.columns,
+        use_mode_colors = true,
+        fmt = function(name, context)
+          -- Show + if buffer is modified in tab
+          local buflist = vim.fn.tabpagebuflist(context.tabnr)
+          local winnr = vim.fn.tabpagewinnr(context.tabnr)
+          local bufnr = buflist[winnr]
+          local mod = vim.fn.getbufvar(bufnr, '&mod')
 
-        return name .. (mod == 1 and ' +' or '')
-      end
+          return name .. (mod == 1 and ' +' or '')
+        end
       }
     }
   },
+  sections = {
+    lualine_c = {
+      {
+        'filename',
+        newfile_status = true,
+        path = 1,
+      }
+    },
+  },
   options = {
-      theme = "gruvbox-material",
+    theme = "gruvbox-material",
   }
 }
