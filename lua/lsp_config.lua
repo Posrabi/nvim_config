4064a39b1af2e85c673468f145a6167853df26a7
Tri Dao <trid@nvidia.com>
2024-08-03 14:45:44 -0400

    Enable lsp for bashls and rust-analyzer

--- lua/lsp_config.lua
+++ lua/lsp_config.lua
@@ -199,6 +199,14 @@ nvim_lsp.clangd.setup {
   capabilities = require('cmp_nvim_lsp').default_capabilities()
 }
 
+nvim_lsp.rust_analyzer.setup {
+  on_attach = on_attach,
+}
+
+nvim_lsp.bashls.setup {
+  on_attach = on_attach,
+}
+
 vim.g.copilot_no_tab_map = true
 vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 vim.g.copilot_filetypes = {
